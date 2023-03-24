4821aba195164175539cd6185e1389364162c105
yagege <wg_fan@qq.com>
2023-03-24 18:03:55 +0800

    update pytest-benchmark

--- smcompiler/smc_benchmark.py
+++ smcompiler/smc_benchmark.py
@@ -1,34 +1,40 @@
-from expression import Secret
-from protocol import ProtocolSpec
-from test_integration import suite, run_processes
+from pytest_benchmark.plugin import benchmark
+
+from smcompiler.expression import Expression, Secret
+from smcompiler.protocol import ProtocolSpec
+
+FIXED_VAL = 20
+CLIENT_NAME_PREFIX = "party_"
 
 
-def run(participants, *clients):
-    results = run_processes(participants, *clients)
-    return results
+def secret_generator(num_party: int) -> dict:
+    party_dict = dict()
+    for num in range(0, num_party):
+        party_dict[CLIENT_NAME_PREFIX + str(num)] = Secret()
+    return party_dict
 
 
-def test_benchmark_suite_1(benchmark):
-    """
-        f(a, b, c) = a + b + c
-        """
+def party_generator(secret_dict):
+    parties = dict()
+    key_list = secret_dict.keys()
+    for key in key_list:
+        parties[key] = {secret_dict[key], FIXED_VAL}
+    return parties
 
-    alice_secret = Secret()
-    bob_secret = Secret()
-    charlie_secret = Secret()
 
-    parties = {
-        "Alice": {alice_secret: 3},
-        "Bob": {bob_secret: 14},
-        "Charlie": {charlie_secret: 2}
-    }
+def expr_generator_add(num_party: int, num_add: int):
+    secret_dict = secret_generator(num_party)
+    parties = party_generator(secret_dict)
+    secret_list = list(secret_dict.keys())
+    expr = secret_list[0]
+    for i in range(0, num_add):
+        expr = expr + secret_list[i % num_party]
+    return parties, expr
 
-    expr = (alice_secret + bob_secret + charlie_secret)
-    expected = 3 + 14 + 2
 
-    participants = list(parties.keys())
-
-    prot = ProtocolSpec(expr=expr, participant_ids=participants)
+def test_party_3_add_50():
+    parties, expr = expr_generator_add(3, 50)
+    prot = ProtocolSpec(expr=expr, participant_ids=list(parties.keys()))
     clients = [(name, prot, value_dict) for name, value_dict in parties.items()]
 
     results = benchmark(run, participants, *clients)
