4bd9fdfa16503aacdf348cec8154dfd7844ed8c1
yagege <wg_fan@qq.com>
2023-03-30 19:25:55 +0800

    update benchmark latest

--- smcompiler/figure/figure_plot.py
+++ smcompiler/figure/figure_plot.py
@@ -4,35 +4,57 @@ import matplotlib.pyplot as plt
 plt.rc('font', family='Times New Roman')
 
 # 字体大小
-font_size = 14
+font_size = 30
 
 # 数据
 party_num = [5, 10, 15, 20, 25, 30]
-comm_cost_request = [1428, 5702, 12834, 22806, 35632, 51296]
-comm_cost_response = [2298, 10310, 24144, 43686, 68982, 100016]
+comm_cost_request_left = [1.428, 5.702, 12.834, 22.806, 35.632, 51.296]
+comm_cost_response_left = [2.298, 10.310, 24.144, 43.686, 68.982, 100.016]
 
-plt.figure(figsize=(8, 5))
-ax = plt.subplot(111)
+comm_cost_request_right = [1.428, 5.702, 12.834, 22.806, 35.632, 51.296]
+comm_cost_response_right = [2.298, 10.310, 24.144, 43.686, 68.982, 100.016]
 
-plt.plot(party_num, comm_cost_request, linewidth = 3, color = 'r', label="Request", marker = 'o', markersize = 8)
-plt.plot(party_num, comm_cost_response, linewidth = 3, color = 'b', label="Response", marker = 'v', markersize = 8)
+plt.figure(figsize=(17, 6))
+ax = plt.subplot(121)
+
+plt.plot(party_num, comm_cost_request_left, linewidth=3, color='r', label="Request", marker='o', markersize=8)
+plt.plot(party_num, comm_cost_response_left, linewidth=3, color='b', label="Response", marker='v', markersize=8)
 
 # 线标签放置在坐上方
-plt.legend(loc="upper left", fontsize = font_size)
+plt.legend(loc="upper left", fontsize=font_size)
 
 # 设置坐标轴标签字号
-plt.xticks(fontsize = font_size)
-plt.yticks(fontsize = font_size)
+plt.xticks(fontsize=font_size)
+plt.yticks(fontsize=font_size)
 
 # 设置坐标轴标签大小
-ax.set_xlabel(..., fontsize = font_size)
-ax.set_ylabel(..., fontsize = font_size)
+ax.set_xlabel(..., fontsize=font_size)
+ax.set_ylabel(..., fontsize=font_size)
 
 # 坐标轴标签
 plt.xlabel("Number of party")
-plt.ylabel("Communication cost (bytes)")
+plt.ylabel("Comm-cost (kilo-bytes)")
 
+ax = plt.subplot(122)
+plt.plot(party_num, comm_cost_request_right, linewidth=3, color='r', label="Request", marker='o', markersize=8)
+plt.plot(party_num, comm_cost_response_right, linewidth=3, color='b', label="Response", marker='v', markersize=8)
+
+# 线标签放置在坐上方
+plt.legend(loc="upper left", fontsize=font_size)
+
+# 设置坐标轴标签字号
+plt.xticks(fontsize=font_size)
+plt.yticks(fontsize=font_size)
+
+# 设置坐标轴标签大小
+ax.set_xlabel(..., fontsize=font_size)
+ax.set_ylabel(..., fontsize=font_size)
+
+# 坐标轴标签
+plt.xlabel("Number of party")
+plt.ylabel("")
+
+plt.subplots_adjust(top=0.95, bottom=0.16, left=0.07, right=0.994, hspace=0, wspace=0.15)
 # 保存文件
 plt.savefig('communication_cost_res_party_5_add_50.png', dpi=600)
-
 plt.show()
